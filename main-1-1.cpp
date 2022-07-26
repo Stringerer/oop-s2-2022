d144350b16a99755c102565433014133970704ac
Stringerer <cstringer@adam.com.au>
2022-07-26 17:46:32 +0930

    fix output message

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,6 +4,6 @@ extern int array_sum(int[], int);
 
 int main() {
     int array[5] = {4,5,6,7,8};
-    std::cout << "The number is: " << array_sum(array, 5) << std::endl;
+    std::cout << "The sum is: " << array_sum(array, 5) << std::endl;
     return 0;
 }
