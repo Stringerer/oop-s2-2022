ab9c1e0bc1fa51e91e2edd46fef1b3946c4a19fa
Stringerer <cstringer@adam.com.au>
2022-07-28 13:52:53 +0930

    forgot to use switch

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 void two_five_nine(int array[], int n){
     int two_count = 0;
-    for (int i = 0; i < n; i++){
-        if (array[i]==2){
-            two_count++;
-        }
-    }
     int five_count = 0;
-    for (int i = 0; i < n; i++){
-        if (array[i]==5){
-            five_count++;
-        }
-    }
     int nine_count = 0;
     for (int i = 0; i < n; i++){
-        if (array[i]==9){
+        switch (array[i])
+        {
+        case 2:
+            two_count++; 
+            break;
+        
+        case 5:
+            five_count++;
+            break;
+        
+        case 9:
             nine_count++;
+            break;
+
+        default:
+            break;
         }
     }
     if(n<1){
