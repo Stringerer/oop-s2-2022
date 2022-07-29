b0f6b0134703dbe4cec9246f2e5ef64f041b9185
Stringerer <cstringer@adam.com.au>
2022-07-29 20:25:14 +0930

    fixed it

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,8 +1,13 @@
 bool is_fanarray(int array[], int n){
     bool is_fan = true;
+    int fan_check = array[0];
     for (int i = 0; i < n; i++){
-        if (array[i] != array[n-1-i]){
+        if (array[i] == array[n-1-i] && array[i] > fan_check){
+            fan_check = array[i];
+        }
+        else{
             is_fan = false;
+            break;
         }
     }
     if (n < 1){
