e359f73b3721364e1de85b7aa3ac5b1665cc0bd9
Stringerer <cstringer@adam.com.au>
2022-07-29 22:33:39 +0930

    fixed mayne

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -1,7 +1,7 @@
 double sum_even(double array[], int n){
     double sum = 0.0;
     for (int i = 0; i < n; i++){
-        if (i%2==1){
+        if (i%2==0){
             sum = sum + array[i];
         }
     }
