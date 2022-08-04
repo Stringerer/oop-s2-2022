a63447d0679aea97837586ee377560b520dd9465
Stringerer <cstringer@adam.com.au>
2022-08-04 22:49:05 +0930

    fix

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -2,19 +2,27 @@
 
 using namespace std;
 
-void print_scaled(int array[3][3],int scale){
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            array[i][j] = array[i][j]*scale;
+void print_scaled(int array[3][3], int scale)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            array[i][j] = array[i][j] * scale;
         }
     }
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-          cout <<array[i][j]<<" ";
-          if (j==2){
-            cout <<endl;
-          }
-            
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (j == 2)
+            {
+                cout << array[i][j] << endl;
+            }
+            else
+            {
+                cout << array[i][j] << " ";
+            }
         }
     }
 }
