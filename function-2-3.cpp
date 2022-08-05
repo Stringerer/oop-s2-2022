a1eccaf7dbc385c76b9b3ef491def12cd3f057cf
Stringerer <cstringer@adam.com.au>
2022-08-05 17:43:41 +0930

    first

---
+++ function-2-3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+bool is_palindrome(int array[], int n){
+    bool is_fan = true;
+    int fan_check = (array[0]-1);
+    for (int i = 0; i < n; i++){
+        if (array[i] == array[n-1-i]){
+        }
+        else{
+            is_fan = false;
+        }
+    
+    }
+    for ( int i = 0; i < (n/2); i++){
+        if (array[i] > fan_check){
+
+        }
+        else{
+            is_fan = false;
+        }
+    }
+    if (n < 1){
+        is_fan = false;
+    }
+    return is_fan;
+}
+
+int sum_array_elements(int array[], int n) {
+    int sum = 0;
+    if (n < 1){;
+        sum = -1;
+    }
+    else{
+        for (int i = 0; i < n; i++){
+            sum = sum + array[i];
+        }
+    }
+    
+    return sum;
+}
+
+int sum_if_palindrome(int integers[], int length){
+    int sum;
+    bool pal_check = is_palindrome(integers,length);
+    if (pal_check == true){
+        sum = sum_array_elements(integers, length);
+    }
+    else{
+        sum = -2;
+    }
+    return sum;
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+extern int sum_if_palindrome(int[], int);
+
+int main() {
+    int array[6]={1,2,3,3,2,1};
+    cout << "Sum is: " << sum_if_palindrome(array,6) << endl;
+    return 0;
+}
