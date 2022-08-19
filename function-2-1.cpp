548bbe88373e87bc915b73f2ac76d49d85658a44
Stringerer <cstringer@adam.com.au>
2022-08-19 17:31:10 +0930

    fix

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void multiples_of_seven(int *nums,int length){
     for (int i = 0; i < length; i++){
         if (*nums%7==0){
-            cout << *nums << " ";
+            cout << *nums << endl;
         }
         *nums++;
     }
