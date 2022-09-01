a9df568561b331723175d0ecafdf6a8e54722ce7
Stringerer <cstringer@adam.com.au>
2022-09-01 14:47:43 +0930

    fix

---
+++ main-1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Capybara.h"
+#include <string>
+
+using namespace std;
+
+int main(){
+    Capybara c1;
+    c1.setName("Gerald");
+    c1.setAge(100);
+    Capybara c2;
+    c2.setName("Big Dog");
+    c2.setAge(12);
+    Capybara c3;
+    c3.setName("Frank");
+    c3.setAge(19);
+    Capybara c4;
+    c4.setName("Balls");
+    c4.setAge(2);
+    return 0;
+}
